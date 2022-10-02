c69bf46c229f0bc72ee5ef0356995ef874768bb4
Edwin Santana <edwsan2001edwsan@gmail.com>
2022-10-02 00:06:39 -0500

    Cambios desntro del test

--- src/test/java/com/example/ob_rest_datajpa/controller/BookControllerTest.java
+++ src/test/java/com/example/ob_rest_datajpa/controller/BookControllerTest.java
@@ -2,6 +2,7 @@ package com.example.ob_rest_datajpa.controller;
 
 import com.example.ob_rest_datajpa.entities.Book;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,7 +30,7 @@ class BookControllerTest {
         restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:"+port);
         testTemplate = new TestRestTemplate(restTemplateBuilder);
     }
-
+    @DisplayName("Comprobar Metodo hello")
     @Test
     void hello() {
         ResponseEntity<String> response = testTemplate.getForEntity("/hola", String.class);
@@ -66,15 +67,22 @@ class BookControllerTest {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
         String  json = """
-                 "title": "Libro Hecho desde Spring Boot",
+                    {
+                    "title": "Libro Hecho desde Spring Test",
                     "author": "LinuxTorvals",
                     "editorial": "Sin Informacion",
                     "pages": 500,
                     "price": 20.10,
                     "relaseDate": "2020-12-10",
                     "online": false
+                    }
                 """;
         HttpEntity<String> request = new HttpEntity<>(json,headers);
         ResponseEntity<Book> response = testTemplate.exchange("/api/books", HttpMethod.POST,request,Book.class);
+
+        Book result = response.getBody();
+
+        assertEquals(1L,result.getId());
+        assertEquals("Libro Hecho desde Spring Test",result.getTitle());
     }
 }
